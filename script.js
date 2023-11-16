86cd4fa117e8868f78e802327eed00c7bf6020d7
Upender-baghel <Upender-baghel@users.noreply.github.com>
2023-11-16 13:26:42 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function btn(){
+
+
+    document.body.style.backgroundColor = "pink"
+
+}
+function check(){
+    document.getElementById('pic2').innerHTML = '<img src="images.jpg" alt="error" id="pic2" > '
+
+    
+}
+
+function lin(){
+    alert("Error")
+
+}
